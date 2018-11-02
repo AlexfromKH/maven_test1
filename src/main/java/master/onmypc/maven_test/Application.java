3ab94cea70367c4313950293270341cc5d8c51c9
AlexfromKH <dekormag@gmail.com>
2018-11-02 17:43:43 +0100

    StringUtils and added dependency

--- src/main/java/master/onmypc/maven_test/Application.java
+++ src/main/java/master/onmypc/maven_test/Application.java
@@ -2,8 +2,14 @@ package master.onmypc.maven_test;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
+
+		public int countWords(String words) {
+				String[] separateWords = StringUtils.split(words, ' ');
+				return (separateWords == null) ? 0 : separateWords.length;
+		}
 
 	  public void greet() {
 	  		List<String> greetings = new ArrayList<>();
@@ -20,8 +26,10 @@ public class Application {
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
-    	 System.out.println ("Starting Application");
-	     Application app = new Application();
-    	 app.greet();
+    		System.out.println ("Starting Application");
+	     	Application app = new Application();
+    	 	app.greet();
+				int count = app.countWords("this is four words");
+				System.out.println ("Word count" + count);
 		}
 }
